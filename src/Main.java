289225d9f82b0f1e8b01f4753cfbd138373292cd
Odirlei <odirlei_sabella@hotmail.com>
2023-10-09 19:10:19 -0300

    adicionado documento de porcentagens

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<PessoaFisica> listaPf = new ArrayList<>();
+        PessoaFisica metodoPf = new PessoaFisica();
+
 
         System.out.println("Bem vindo ao sistema de cadastro de Pessoas Físicas e Juridicas");
 
@@ -25,11 +32,74 @@ public class Main {
 
                         switch (opcaoPf){
                             case "1":
-                                System.out.println("Cadastrar PF");
+
+                                PessoaFisica novaPf = new PessoaFisica();
+                                Endereco novoEndPf = new Endereco();
+
+                                System.out.println("Digite o nome:");
+                                novaPf.nome = leitor.nextLine();
+
+                                System.out.println("Digite o cpf:");
+                                novaPf.cpf = leitor.nextLine();
+
+                                System.out.println("Digite o rendimento:");
+                                novaPf.rendimento = leitor.nextFloat();
+
+                                System.out.println("Digite a data de nascimento: (dd/mm/aaaa)");
+                                novaPf.dataNasc = LocalDate.parse(leitor.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                                Period idade = Period.between(novaPf.dataNasc, LocalDate.now());
+
+                                if (idade.getYears() >= 18){
+                                    System.out.println("Idade Válida!");
+                                } else {
+                                    System.out.println("Idade Inválida!");
+                                    break;
+                                }
+
+                                System.out.println("Digite o logradouro");
+                                novoEndPf.logradouro = leitor.next();
+
+                                System.out.println("Digite o numero");
+                                novoEndPf.numero = leitor.nextInt();
+
+                                System.out.println("Este endereço é comercial? S/N:");
+                                String endCom = leitor.next();
+
+                                if (endCom.equals("S") || endCom.equals("s")){
+                                    novoEndPf.endComercial = true;
+                                } else {
+                                    novoEndPf.endComercial = false;
+                                }
+
+                                novaPf.endereco = novoEndPf;
+
+                                listaPf.add(novaPf);
+
+                                System.out.println("Cadastro realizado com sucesso!");
+
                                 break;
 
                             case "2":
-                                System.out.println("Lista PF");
+
+                                if (listaPf.size() > 0){
+
+                                    for (PessoaFisica cadaPf : listaPf) {
+                                        System.out.println("Nome: " + cadaPf.nome);
+                                        System.out.println("CPF: " + cadaPf.cpf);
+                                        System.out.println("Data de Nascimento: " + cadaPf.dataNasc.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                                        System.out.println("Imposto a ser pago: " + metodoPf.CalcularImposto(cadaPf.rendimento));
+                                        System.out.println("Endereço: " + cadaPf.endereco.logradouro + "-" + cadaPf.endereco.numero);
+
+                                        System.out.println();
+                                        System.out.println("Aperte ENTER para continuar");
+                                        leitor.nextLine();
+                                    }
+
+                                } else {
+                                    System.out.println("Lista vazia!");
+                                }
+
                                 break;
 
                             case "0":
